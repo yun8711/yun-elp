fc8ce164f89aed1ec6f3d7469838d29ce90c2e00
liuyun <liuyun.dev@qq.com>
2025-06-28 00:53:42 +0800

    test(play): :white_check_mark: 优化基础配置，忽略play子项目部分文件

--- play/components.d.ts
+++ play/components.d.ts
@@ -7,23 +7,12 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    ElButton: typeof import('element-plus/es')['ElButton']
-    ElCascader: typeof import('element-plus/es')['ElCascader']
     ElColorPicker: typeof import('element-plus/es')['ElColorPicker']
     ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
-    ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
     ElIcon: typeof import('element-plus/es')['ElIcon']
-    ElInput: typeof import('element-plus/es')['ElInput']
-    ElInputNumber: typeof import('element-plus/es')['ElInputNumber']
     ElOption: typeof import('element-plus/es')['ElOption']
-    ElRadio: typeof import('element-plus/es')['ElRadio']
-    ElRadioGroup: typeof import('element-plus/es')['ElRadioGroup']
     ElSelect: typeof import('element-plus/es')['ElSelect']
     ElSwitch: typeof import('element-plus/es')['ElSwitch']
-    ElText: typeof import('element-plus/es')['ElText']
-    GridCanvas: typeof import('./src/components/GridCanvas.vue')['default']
     Layout: typeof import('./src/components/Layout.vue')['default']
-    YAppWrap: typeof import('yun-elp/es/components')['YAppWrap']
-    YLabel: typeof import('yun-elp/es/components')['YLabel']
   }
 }
--- play/src/App.vue
+++ play/src/App.vue
@@ -5,46 +5,6 @@
 
       <div>当前语言：{{ locale }}</div>
 
-      <div class="demo-block">
-        <h3>基础用法</h3>
-        <y-label label="用户名">老刘</y-label>
-        <y-label label="用户名" colon=":">老刘</y-label>
-      </div>
-
-      <div class="demo-block">
-        <h3>带图标</h3>
-        <y-label label="用户名" colon=":">
-          <template #prefix>
-            <el-icon>
-              <User />
-            </el-icon>
-          </template>
-          老刘
-          <template #suffix>
-            <el-icon>
-              <InfoFilled />
-            </el-icon>
-          </template>
-        </y-label>
-      </div>
-
-      <div class="demo-block">
-        <h3>基础用法</h3>
-        <y-label label="用户名" colon=":" height="80px" :label-style="labelStyle" :content-style="contentStyle">
-          Yingdu Building, Zhichun Road, BeijingYingdu Building, Zhichun Road, BeijingYingdu Building, Zhichun Road,
-          BeijingYingdu Building, Zhichun Road, BeijingYingdu Building, Zhichun Road, BeijingYingdu Building, Zhichun
-          Road, Beijing
-        </y-label>
-      </div>
-
-      <div class="demo-block">
-        <h3>表单控件组合</h3>
-        <div style="display: flex;flex-direction: column;gap: 8px;">
-          <y-label label="Name" width="300px" label-width="100px" label-align="right">Socrates</y-label>
-          <y-label label="Mobile" width="300px" label-width="100px" label-align="right"> 123-1234-1234</y-label>
-          <y-label label="Residence" width="300px" label-width="100px" label-align="right">Beijing</y-label>
-        </div>
-      </div>
     </y-app-wrap>
   </Layout>
 </template>
@@ -63,19 +23,6 @@ const appWrapConfig = {
   }
 }
 
-const labelStyle = {
-  color: 'red',
-  backgroundColor: '#eee',
-  justifyContent: 'flex-start',
-  alignItems: 'flex-start'
-}
-
-const contentStyle = {
-  color: 'blue',
-  backgroundColor: '#eee',
-  justifyContent: 'flex-end',
-  alignItems: 'flex-end'
-}
 </script>
 
 <style lang="scss" scoped>
